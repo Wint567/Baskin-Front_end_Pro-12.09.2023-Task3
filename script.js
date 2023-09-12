cf546663967499f3537783d1f15243bf22c8bf2d
Wint567 <134635875+Wint567@users.noreply.github.com>
2023-09-12 19:01:05 +0300

    task3

---
+++ script.js
@@ -0,0 +1,7 @@
+var num1 = parseFloat(prompt("Введіть перше число:"));
+var num2 = parseFloat(prompt("Введіть друге число:"));
+
+alert(num1 + " + " + num2 + " = " + (num1 + num2));
+alert(num1 + " - " + num2 + " = " + (num1 - num2));
+alert(num1 + " * " + num2 + " = " + (num1 * num2));
+alert(num1 + " / " + num2 + " = " + (num1 / num2));
